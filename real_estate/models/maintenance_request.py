8eb2983511f76f5e42886e5c2a83a9161b638d0e
codebyhussein <husseinsalah2528@gmailcom>
2026-09-20 00:32:43 +0300

    Add maintenance request model, wizard, and views; implement submission logic and notifications

--- real_estate/models/maintenance_request.py
+++ real_estate/models/maintenance_request.py
@@ -3,12 +3,12 @@ from odoo import models, fields,api
 class MaintenanceRequest(models.Model):
     _name = 'maintenance.request'
     _description = 'Property Maintenance Request'
-    # _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     
     name = fields.Char()
     lease_id = fields.Many2one('real_estate.lease')
-    # tenant_id = fields.Many2one(related='lease_id.tenant_id', store=True)
-    # property_id = fields.Many2one(related='lease_id.property_id', store=True)
+    tenant_id = fields.Many2one(related='lease_id.tenant_id', store=True)
+    property_id = fields.Many2one(related='lease_id.property_id', store=True)
     
     issue_type = fields.Selection([
         ('plumbing', 'Plumbing'),
@@ -17,17 +17,26 @@ class MaintenanceRequest(models.Model):
         ('appliance', 'Appliance'),
         ('other', 'Other')
     ], required=True)
-    description = fields.Text(required=True, tracking=True)
+    description = fields.Text(required=True, )
     urgency = fields.Selection([
         ('low', 'Low'),
         ('medium', 'Medium'),
         ('high', 'High'),
         ('emergency', 'Emergency')
     ], default='medium', required=True)
-    # assigned_to = fields.Many2one('res.users', string='Assigned To')
+    assigned_to = fields.Many2one('res.users', string='Assigned To')
     scheduled_date = fields.Date()
     completion_date = fields.Date()
     actual_cost = fields.Float()
+    preferred_date = fields.Date()
+    tenant_phone = fields.Char()
+    state = fields.Selection([
+    ('submitted', 'Submitted'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+    ('cancelled', 'Cancelled'),
+], string='Status', default='submitted', tracking=True)
+
 
 
     @api.model
