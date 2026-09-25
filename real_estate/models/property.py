08daf42e9c3f9663e87b3b34b48a5bf2483e57a1
codebyhussein <husseinsalah2528@gmailcom>
2026-09-25 23:43:31 +0300

    added image field in model and refactor code

--- real_estate/models/property.py
+++ real_estate/models/property.py
@@ -1,99 +1,4 @@
 
-from re import A
-
-
-from odoo import models, fields, api
-from odoo.exceptions import UserError
-
-class Property(models.Model):
-    _name = 'real_estate.property'
-    _description = 'Real Estate Property'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    
-
-    name = fields.Char(string='Property Name', required=True, index=True)
-    description = fields.Text(string='Description')
-    price = fields.Float(string='Monthly Rent', required=True)    
-    bedrooms = fields.Integer(string='Bedrooms', required=True)
-    available = fields.Boolean(string='Available', default=True, index=True)   
-
-    agent_id = fields.Many2one('res.users', string='Sales Person',)
-    property_type = fields.Selection([
-        ('apartment', 'Apartment'),
-        ('house', 'House'),
-        ('villa', 'Villa'),
-        ('commercial', 'Commercial'),
-    ], string='Property Type', required=True)
-    deposit = fields.Float(required=True)
-    lease_ids =fields.One2many('real_estate.lease','property_id' ,string='Leases')
-
-
-    def mark_as_occupied(self):
-        """Mark property as no longer available"""
-        for record in self:
-            record.write({'available': False, 'price': record.price + 1000})
-    
-    def mark_as_available(self):
-        """Mark property as available"""
-        for record in self:
-            record.write({'available': True})
-
-
-
-    def add_text(self):
-        """Add text to the description field"""
-        for record in self:
-            record.write({'description': (record.description + ' Description ')})
-
-    def increse_deposit(self):
-        """Increase deposit by 10%"""
-        for record in self:
-            record.write({'deposit': record.deposit +1000})
-
-
-
-    def add_bedroom(self):
-        """Add a bedroom to the property"""
-        for record in self:
-            record.write({'bedrooms': record.bedrooms + 1})
-
-
-    def mark_as_villa(self):
-        """Change the property type to Villa"""
-        for record in self:
-            if record.available:
-                record.write({'property_type': 'villa'})
-
-
-    def get_agent_name(self):
-         for record in self:
-
-                record.write({'description': record.agent_id.name})  # pyright: ignore[reportAttributeAccessIssue]
-
-    
-
-    # def write(self, vals):
-    #      if vals.get('available')==False :
-    #         raise UserError("You cannot edit bedrooms when they are unavailable.")
-    #      return super(Property,self).write(vals)
-
-    # def action_open_lease_wizard(self):
-    #    self.ensure_one()
-
-    #    return {
-    #     'type': 'ir.actions.act_window',
-    #     'name': 'Create Lease',
-    #     'res_model': 'real_estate.lease.wizard',
-    #     'view_mode': 'form',
-    #     'target': 'new',
-    #     'context': {
-    #         'default_property_id': self.id,
-    #         'default_monthly_rent': self.price,
-    #         'default_user_id': self.env.user.id,
-    #     },
-    # }
-    
-    
 from odoo import models, fields
 
 
@@ -126,6 +31,10 @@ class Property(models.Model):
         string='Available',
         default=True,
         index=True
+    )
+
+    image = fields.Binary(
+        string='Property Image'
     )
 
     agent_id = fields.Many2one(
@@ -195,3 +104,4 @@ class Property(models.Model):
             record.write({
                 'description': record.agent_id.name
             })
+
